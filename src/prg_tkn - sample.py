264911fc979cb1904fdc816a1dc1cdabe335f7db
slong <zoharslong@hotmail.com>
2021-06-02 16:06:15 +0800

    update

---
+++ src/prg_tkn - sample.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2021-06-01
+token and secrete keys
+@author: zoharslong
+"""
+
+dct_jgh = {
+    'neek': '',
+    'appkey': ''
+}   # 极光proxy卡密
+dct_sql = {
+    'hst': '',
+    'prt': 00000,
+    'usr': '',
+    'psw': ''
+}   # 腾讯云mysql卡密
+str_mng = 'mongodb://username:passwords@000.000.0.0:27017/admin'  # mongodb卡密
